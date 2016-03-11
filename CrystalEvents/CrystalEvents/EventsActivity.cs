234c680afb55d5dba92694aa458fe5296a3c8879
Tinnevdabb <tinne.vdabb@skynet.be>
2016-03-11 16:33:50 +0100

    Added events activity

---
+++ CrystalEvents/CrystalEvents/EventsActivity.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace CrystalEvents
+{
+
+    [Activity()]
+    class EventsActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Events);
+
+            
+        }
+    }
+}
